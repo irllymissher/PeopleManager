578d3b88dd7b720f0b2ec685e83c9e8cb267ddd7
alumno <alumno@A01324>
2026-04-24 12:59:59 +0200

    feat: creado Enum
    Se hace con el objetivo de evitar magic String y mejorar el tipado evitando
    posible errores.

---
+++ peoplemanager/src/peopleManager/Categoria.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package peopleManager;
+
+/**
+ *
+ * @author alumno
+ */
+public enum Categoria {
+    DEVELOPER,
+    ANALYST,
+    MANAGER,
+    BACKEND,
+    DEVOPS,
+    TECHLEAD,
+    CTO
+}
