f7622f6bf1b43ec7bf921c7441607358e277240d
alumno <alumno@A01324>
2026-04-24 12:54:09 +0200

    feat: Clase empleado añadida.

---
+++ peoplemanager/src/peopleManager/Empleado.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package peopleManager;
+
+/**
+ *
+ * @author alumno
+ */
+public class Empleado {
+    public String objectId;
+    public String nombre;
+    public String apellido;
+    public String antiguedad;
+    // todo categoria
+
+    public Empleado(String objectId, String nombre, String apellido, String antiguedad) {
+        this.objectId = objectId;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.antiguedad = antiguedad;
+        // todo categoria
+    }
+    
+    
+}
