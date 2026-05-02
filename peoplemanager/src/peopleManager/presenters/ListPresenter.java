71cbb29ce7a34499170f9d23bea8cbe33f6c9fe7
irllymissher <tomascalderon@proton.me>
2026-05-02 20:07:23 +0200

    feat: se han añadido los metodos (CRUD) para el presenetador

--- peoplemanager/src/peopleManager/presenters/ListPresenter.java
+++ peoplemanager/src/peopleManager/presenters/ListPresenter.java
@@ -7,4 +7,35 @@ import peopleManager.IListView;
 public class ListPresenter {
     private IListView view;
     private ArrayList<Empleado> empleados;
+    
+    public ListPresenter(IListView view){
+        this.view = view;
+    }
+    
+    private void cargarDatos(ArrayList<Empleado> empleados){
+        this.empleados = empleados;
+        this.actualizarVista();
+        this.view.abrirLista();
+    }
+    
+    public void actualizarVista(){
+        this.view.cargarFilaEmpleados(this.empleados);
+    }
+    
+    public void actualizarEmpleado(Empleado empleadoActualizado){
+        String id = empleadoActualizado.objectId;
+        for (int i = 0; i < empleados.size(); i++) {
+            Empleado empleado = empleados.get(i);
+            if (empleado.equals(id)){
+                empleados.set(i, empleadoActualizado);
+                break;
+            }
+        }
+        this.actualizarVista();
+    }
+    
+    public void insertarEmpleado(Empleado empleado){
+        return;
+    }
+    
 }
