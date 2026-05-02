363b1ac9c96f531e29378d2ad583f5fa5515a381
irllymissher <tomascalderon@proton.me>
2026-05-02 20:25:40 +0200

    feat: Se añade depedencias a metodo run para ejecutar la app

--- peoplemanager/src/peopleManager/App.java
+++ peoplemanager/src/peopleManager/App.java
@@ -4,6 +4,10 @@
  */
 package peopleManager;
 
+import java.util.ArrayList;
+import peopleManager.Categoria;
+import peopleManager.presenters.ListPresenter;
+
 /**
  *
  * @author alumno
@@ -12,7 +16,13 @@ public class App {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ListFrame().setVisible(true);
+                ArrayList<Empleado> empleado = new ArrayList<Empleado>();
+                empleado.add(new Empleado("1","Bob","Smith","5",Categoria.CTO));
+                empleado.add(new Empleado("2","John","Doe","10",Categoria.MANAGER));
+                
+                ListFrame listView = new ListFrame();
+                ListPresenter listPresenter = new ListPresenter(listView);
+                listPresenter.mostrarEmpleados();
             }
         });
     }
