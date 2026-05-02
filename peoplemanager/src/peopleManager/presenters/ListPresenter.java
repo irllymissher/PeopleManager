7a61adf10e042e22d83bc09f21760a05f49b7a53
irllymissher <tomascalderon@proton.me>
2026-05-02 19:39:48 +0200

    feat: se añade el presentador de la lista al que se le añaden las dependecias: modelo y vista

---
+++ peoplemanager/src/peopleManager/presenters/ListPresenter.java
@@ -0,0 +1,10 @@
+package peopleManager.presenters;
+
+import java.util.ArrayList;
+import peopleManager.Empleado;
+import peopleManager.IListView;
+
+public class ListPresenter {
+    private IListView view;
+    private ArrayList<Empleado> empleados;
+}
