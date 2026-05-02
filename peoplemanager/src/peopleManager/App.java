e6d683d60e6f61a49a45b55bd011019c00aefcca
irllymissher <tomascalderon@proton.me>
2026-05-02 20:45:22 +0200

    fix: se cargan correctamente los empleados

--- peoplemanager/src/peopleManager/App.java
+++ peoplemanager/src/peopleManager/App.java
@@ -16,13 +16,13 @@ public class App {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ArrayList<Empleado> empleado = new ArrayList<Empleado>();
-                empleado.add(new Empleado("1","Bob","Smith","5",Categoria.CTO));
-                empleado.add(new Empleado("2","John","Doe","10",Categoria.MANAGER));
+                ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+                empleados.add(new Empleado("1","Bob","Smith","5",Categoria.CTO));
+                empleados.add(new Empleado("2","John","Doe","10",Categoria.MANAGER));
                 
                 ListFrame listView = new ListFrame();
                 ListPresenter listPresenter = new ListPresenter(listView);
-                listPresenter.mostrarEmpleados();
+                listPresenter.cargarDatos(empleados);
             }
         });
     }
