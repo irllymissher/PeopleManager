206f231ecbf8eb6dccf337e0d295e890b7164159
irllymissher <tomascalderon@proton.me>
2026-05-06 14:41:15 +0200

    feat: se añade una nueva Vista consola

---
+++ peoplemanager/src/peopleManager/views/ListConsoleView.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peopleManager.views;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class ListConsoleView implements IListView{
+
+    private Runnable nuevaAccion;
+    private Runnable accionSeleccionada;
+    private String idSeleccionado;
+    private Scanner sc = new Scanner(System.in);
+    
+    @Override
+    public void abrirLista() {
+        System.out.println("*** LIST ***");
+    }
+
+    @Override
+    public String obtenerIdDelEmpleadoSeleccionado() { return this.idSeleccionado; }
+
+    @Override
+    public void asignarAccionAlSeleccionarEmpleado(Runnable accionSeleccionar) { this.accionSeleccionada = accionSeleccionada; }
+
+    @Override
+    public void asignarAccionParaNuevoEmpleado(Runnable accionNuevoEmpleado) { this.nuevaAccion = nuevaAccion; }
+
+    @Override
+    public void asignarFilaDeEmpleado(ArrayList<FilaEmpleado> filaDeEmpleados){
+        for (FilaEmpleado fila : filaDeEmpleados) {
+            System.out.println("\n--- LISTA DE EMPLEADOS ---");
+            
+            System.out.println(String.format("ID: %s | Nombre: %s | Antigüedad: %s | Categoría: %s | Salario: %s",
+                fila.EmpleadoId, fila.NombreCompleto, fila.Antiguedad, fila.Categoria, fila.Salario)); 
+        }
+        System.out.println("--------------------------------------------------");
+        System.out.println("Introduce el ID del empleado para ver detalles");
+        
+        String id = sc.nextLine();
+        
+        this.idSeleccionado = id;
+        accionSeleccionada.run(); 
+    }
+    
+}
