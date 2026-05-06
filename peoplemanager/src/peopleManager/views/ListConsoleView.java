b597c7b31b3203037d3b003b438fcf389f7bc50c
irllymissher <tomascalderon@proton.me>
2026-05-06 15:03:58 +0200

    fix: quitando comentarios

--- peoplemanager/src/peopleManager/views/ListConsoleView.java
+++ peoplemanager/src/peopleManager/views/ListConsoleView.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package peopleManager.views;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author alumno
- */
 public class ListConsoleView implements IListView{
 
     private Runnable nuevaAccion;
@@ -47,7 +38,7 @@ public class ListConsoleView implements IListView{
         String id = sc.nextLine();
         
         this.idSeleccionado = id;
-        accionSeleccionada.run(); 
+        accionSeleccionada.run();
     }
     
 }
