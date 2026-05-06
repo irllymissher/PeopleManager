a68f6e3f6648ac9e347efdda2e668563fd779871
irllymissher <tomascalderon@proton.me>
2026-05-06 13:40:38 +0200

    fix: se añade el apellido porque el formulario detalle tiene cajas separadas

--- peoplemanager/src/peopleManager/views/FormularioEmpleado.java
+++ peoplemanager/src/peopleManager/views/FormularioEmpleado.java
@@ -1,15 +1,17 @@
 package peopleManager.views;
 
 public class FormularioEmpleado {
-        public String EmpleadoId;
-    public String NombreCompleto;
+    public String EmpleadoId;
+    public String Nombre;
+    public String Apellido;
     public String Antiguedad;
     public String Salario;
     public String Categoria;
 
-    public FormularioEmpleado(String EmpleadoId, String NombreCompleto, String Antiguedad, String Salario, String Categoria) {
+    public FormularioEmpleado(String EmpleadoId, String Nombre, String Apellido, String Antiguedad, String Salario, String Categoria) {
         this.EmpleadoId = EmpleadoId;
-        this.NombreCompleto = NombreCompleto;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
         this.Antiguedad = Antiguedad;
         this.Salario = Salario;
         this.Categoria = Categoria;
