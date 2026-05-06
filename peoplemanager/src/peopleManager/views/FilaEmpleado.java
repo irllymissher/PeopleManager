3ea72a6978848d71d7846d9dd244ed6e94e2e68f
irllymissher <tomascalderon@proton.me>
2026-05-06 12:45:09 +0200

    feat: Se añade la clase FilaEmpleado para representar el modelo de un componente de la vista

---
+++ peoplemanager/src/peopleManager/views/FilaEmpleado.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peopleManager.views;
+
+/**
+ *
+ * @author alumno
+ */
+public class FilaEmpleado {
+    public String EmpleadoId;
+    public String NombreCompleto;
+    public String Antiguedad;
+    public String Salario;
+    public String Categoria;
+
+    public FilaEmpleado(String EmpleadoId, String NombreCompleto, String Antiguedad, String Salario, String Categoria) {
+        this.EmpleadoId = EmpleadoId;
+        this.NombreCompleto = NombreCompleto;
+        this.Antiguedad = Antiguedad;
+        this.Salario = Salario;
+        this.Categoria = Categoria;
+    }
+}
